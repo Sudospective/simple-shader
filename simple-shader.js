fa4d20afee9763a70ecb3a2f27152febb2589a57
Sudospective <admin@sudospective.net>
2023-12-11 15:07:36 -0800

    bad extra space icky

--- simple-shader.js
+++ simple-shader.js
@@ -242,7 +242,6 @@ export class SimpleShader {
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
                 gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-                  
               });
             };
             image.onload = assignTexture(unis[sampler[0]]);
